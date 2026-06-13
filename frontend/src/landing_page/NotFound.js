3b9bcaf4143201942fcade0dfa65338b43d67b57
unzila <unzilasaifi75@gmail.com>
2026-06-13 19:33:51 +0530

    add footer component

---
+++ frontend/src/landing_page/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function NotFound() {
+    return (  
+        <div className='container'>
+          <div className='d-flex flex-column justify-content-center align-items-center text-center pt-5 mt-4'> 
+         <h3 >404 Not Found Kiaan couldn’t find that page</h3>
+        <p>We couldn’t find the page you were looking for. Visit Zerodha’s home page</p>
+        </div>
+        </div>
+    );
+}
+
+export default NotFound;
---
+++ frontend/src/landing_page/OpenAccount.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function OpenAccount() {
+    return (  
+        <div className='container'>
+          <div className='d-flex flex-column justify-content-center align-items-center text-center pt-5 mt-4'> 
+         <h3 >Open a Zerodha account</h3>
+        <p >Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades</p>
+        <button className='btn btn-primary'style={{width:"25%", margin:"0 auto"}}>Sign Up for free</button>
+        </div>
+        </div>
+    );
+}
+
+export default OpenAccount;
